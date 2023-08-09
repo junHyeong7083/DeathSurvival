3e97923ffb6f51365fbc6903dd602cc6f34436dc
junHyeong7083 <parkjunhyeong0902@gmail.com>
2023-08-09 23:58:17 +0900

    skilltree linerenderer(ui)
    skilltree linerenderer 표시하기

--- Assets/Script/UITest.cs
+++ Assets/Script/UITest.cs
@@ -9,6 +9,8 @@ public class UITest : MonoBehaviour
     public Button[] firstBtn;
     public Button[] secondBtn;
 
+    RectTransform lastClickBtn;
+    int lastClickIndex;
 
     public Image PlayerIcon;
 
@@ -26,6 +28,8 @@ public class UITest : MonoBehaviour
     private void Start()
     {
         StartCoroutine(SkillStartSet());
+
+
     }
 
 
@@ -75,7 +79,7 @@ public class UITest : MonoBehaviour
             }
             yield return null;
         }
-    }
+    } // 두번째 필드 투명도 조절
 
     public void PointOneSkill()
     {
@@ -86,6 +90,29 @@ public class UITest : MonoBehaviour
 
             for (int i = 0; i < firstBtn.Length; i++)
             {
+                if(ButtonName == (i+1).ToString())
+                {  
+                    LineRenderer playerLine = PlayerIcon.GetComponent<LineRenderer>();
+                    playerLine.positionCount = 2; // 선은 두 개의 점1으로 이루어집니다.
+                    playerLine.startWidth = 0.2f; // 선의 시작 너비
+                    playerLine.endWidth = 0.2f; // 선의 끝 너비
+                  //  playerLine.startColor = Color.white; // 선의 시작 색상
+                  //  playerLine.endColor = Color.white; // 선의 끝 색상
+
+                    /*     Vector3 firstPos = PlayerIcon.rectTransform.position;
+                         Vector3 secondPos = firstBtn[i].rectTransform.position; */
+                    Vector3 firstPos =( PlayerIcon.rectTransform.position);
+
+                    RectTransform rect = firstBtn[i].GetComponent<RectTransform>();
+                    Vector3 secondPos = ( rect.position);
+                  //  RectTransformUtility.WorldToScreenPoint(null, secondPos);
+
+                    playerLine.SetPosition(0, firstPos);
+                    playerLine.SetPosition(1, secondPos);
+                    lastClickIndex = i;
+                    Debug.Log("index : " + lastClickIndex);
+                }
+
                 if (ButtonName != (i + 1).ToString())
                 {
                     Color newColor = firstColor[i];
@@ -102,13 +129,42 @@ public class UITest : MonoBehaviour
 
     public void PointTwoSkill()
     {
-        if(canSecond)
+        if(canSecond && !canFirst)
         {
             string ButtonName = EventSystem.current.currentSelectedGameObject.name;
             Debug.Log(ButtonName);
 
             for (int i = 0; i < secondBtn.Length; i++)
             {
+                if(ButtonName == (i+1).ToString())
+                {
+                    LineRenderer firstBtnLine = firstBtn[lastClickIndex].GetComponent<LineRenderer>();
+                    firstBtnLine.positionCount = 2; // 선은 두 개의 점으로 이루어집니다.
+                    firstBtnLine.startWidth = 0.2f; // 선의 시작 너비
+                    firstBtnLine.endWidth = 0.2f; // 선의 끝 너비
+                 //   firstBtnLine.startColor = Color.white; // 선의 시작 색상
+                 //   firstBtnLine.endColor = Color.white; // 선의 끝 색상
+                                                       //    Vector3 firstPos =( PlayerIcon.rectTransform.position);
+
+                    //  Vector3 firstPos = (PlayerIcon.rectTransform.position);
+                    //  RectTransform rect = firstBtn[i].GetComponent<RectTransform>();
+                    //
+                    //  Vector3 secondPos = (rect.position);
+
+
+
+                    RectTransform rectfirst = firstBtn[lastClickIndex].GetComponent<RectTransform>();
+                    Vector3 firstPos = rectfirst.position;
+
+                    RectTransform rect = secondBtn[i].GetComponent<RectTransform>();
+                    Vector3 secondPos =rect.position;
+
+                    firstBtnLine.SetPosition(0, firstPos);
+                    firstBtnLine.SetPosition(1, secondPos);
+
+                    lastClickIndex = i;
+                }
+
                 if (ButtonName != (i + 1).ToString())
                 {
                     Color newColor = secondColor[i];
@@ -127,6 +183,8 @@ public class UITest : MonoBehaviour
     private void Update()
     {
         skillPointTxt.text = "Point : " + skillPoint.ToString();
+
+
     }
 
 
