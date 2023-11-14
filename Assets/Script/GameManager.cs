1e3e161dc7ff19f658443f6d3096c811e782a01e
junHyeong7083 <parkjunhyeong0902@gmail.com>
2023-11-14 21:47:04 +0900

    Resolution Size Controller
    해상도 사이즈 조절기능

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,17 +4,84 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private void Awake()
+    #region Test
+    int currentScreenIndex = 0;
+    bool isfullScreen = false;
+    public void BasicScreen()
     {
-        Animator[] allAnimators = FindObjectsOfType<Animator>();
-
-        // 각 Animator 컴포넌트에서 현재 재생 중인 모든 애니메이션을 종료합니다.
-        foreach (Animator animator in allAnimators)
-        {
-            animator.speed = 1f; // 애니메이션 재생 속도를 0으로 설정하여 멈춥니다.
-        }
+        currentScreenIndex = 0;
+        if(!isfullScreen)
+           Screen.SetResolution(640, 360, false);
+        if (isfullScreen)
+            Screen.SetResolution(640, 360, true);
     }
 
+    public void TwoScreen()
+    {
+        currentScreenIndex = 1;
+        if(!isfullScreen)
+            Screen.SetResolution(960, 540, false);
+        if (isfullScreen)
+            Screen.SetResolution(960, 540, true);
+    }
+
+    public void ThreeScreen()
+    {
+        currentScreenIndex = 2;
+        if(!isfullScreen)
+            Screen.SetResolution(1280, 720, false);
+        if(isfullScreen)
+            Screen.SetResolution(1280, 720, true);
+    }
+
+    public void FullScreen()
+    {
+        if(!isfullScreen)
+        {
+            isfullScreen = true;
+            Debug.Log("!!");
+            switch (currentScreenIndex)
+            {
+                case 0:
+                    Screen.SetResolution(640, 360, true);
+                    break;
+
+                case 1:
+                    Screen.SetResolution(960, 540, true);
+                    break;
+
+                case 2:
+                    Screen.SetResolution(1280, 720, true);
+                    break;
+            }
+        }
+        else if(isfullScreen)
+        {
+            isfullScreen = false;
+            Debug.Log("!!222");
+            switch (currentScreenIndex)
+            {
+                case 0:
+                    Screen.SetResolution(640, 360, false);
+                    break;
+
+                case 1:
+                    Screen.SetResolution(960, 540, false);
+                    break;
+
+                case 2:
+                    Screen.SetResolution(1280, 720, false);
+                    break;
+            }
+        }
+
+    }
+    #endregion
+
+    private void Start()
+    {
+        //Screen.SetResolution(640, 360, false);
+    }
     void Update()
     {
 
